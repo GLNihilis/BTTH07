286e4d2ce55ba238edea9296fe70d819d60fec9b
NguyenVanCong2051063627 <2051063627@e.tlu.edu.vn>
2022-03-29 21:44:18 +0700

    Country+Address+Email

--- js/script.js
+++ js/script.js
@@ -77,4 +77,84 @@ $(document).ready(function()
             $("#statusOfUserName").css('color','red')
         }
     })
+       //Check ZipCode
+       function checkZipCode()
+       {
+           let zipCode = $('#txtZip').val();
+           let zipCodeRegex = /^[0-9]+$/;
+           if(zipCodeRegex.test(zipCode))
+           {
+               return true
+           }
+           return false
+       }
+       $("#btnRegister").click(function(e)
+       {
+           e.preventDefault();
+           if(checkZipCode())
+           {
+               $("#statusOfZip").text('Zip Code correct')
+               $("#statusOfZip").css('color','blue')
+           }
+           else
+           {
+               $("#statusOfZip").text('Must be numeric only')
+               $("#statusOfZip").css('color','red')
+           }
+       })
+   
+       //Check Email
+       function checkEmail()
+       {
+           let email = $('#txtEmail').val();
+           let emailRegex = /^\b[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b$/i;
+           if(emailRegex.test(email))
+           {
+               return true
+           }
+           return false
+       }
+       $("#btnRegister").click(function(e)
+       {
+           e.preventDefault();
+           if(checkEmail())
+           {
+               $("#statusOfEmail").text('Email correct')
+               $("#statusOfEmail").css('color','blue')
+           }
+           else
+           {
+               $("#statusOfEmail").text('Email incorrect')
+               $("#statusOfEmail").css('color','red')
+           }
+       })
+          //Check Country
+    function checkCountry()
+    {
+        let country = $('#selectCountry').val();
+        if(country == '')
+        {
+            return true
+        }
+        else
+        {
+            return false
+        }
+    }
+    $("#btnRegister").click(function(e)
+    {
+        e.preventDefault();
+        if( checkCountry() == true)
+        {
+            $("#statusOfCountry").text('Select a country')
+            $("#statusOfCountry").css('color','red')
+            $("#statusOfCountry").css('font-size','15px')
+        }
+        else
+        {
+            $("#statusOfCountry").text('Country is valid')
+            $("#statusOfCountry").css('color','blue')
+            $("#statusOfCountry").css('font-size','15px')
+        }
+    })
 })
